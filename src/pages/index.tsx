3e41616f9e09a0f3f30322a976e7ae08f26b2719
fabricio cunha <fabriciocunhadeveloper@gmail.com>
2021-04-28 02:48:54 +0100

    Done challengeBox styling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import { CompletedChallenges } from "../components/CompletedChallenges";
 import { Countdown } from "../components/Countdown";
 import { ExperienceBar } from "../components/ExperienceBar";
+import { ChallengeBox } from "../components/ChallengeBox";
 import { Profile } from "../components/Profile";
 
 import Head from 'next/head';
@@ -21,6 +22,7 @@ export default function Home() {
           <Countdown />
         </div>
         <div>
+          <ChallengeBox />
         </div>
       </section>
     </div>
